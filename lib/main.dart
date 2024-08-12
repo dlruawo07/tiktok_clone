972852404dabce9deb11b2e39a238d8ef9081716
dlruawo07 <dlruawo07@gmail.com>
2024-08-12 14:57:23 +0900

    ~9

--- lib/main.dart
+++ lib/main.dart
@@ -15,32 +15,36 @@ class TikTokApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        primaryColor: const Color(0xFFE9435A),
-        textSelectionTheme: const TextSelectionThemeData(
-          cursorColor: Color(0xFFE9435A),
-        ),
-        // NOTE: 모든 Scaffold의 앱바에 적용되는 테마
-        appBarTheme: const AppBarTheme(
-          foregroundColor: Colors.black,
-          backgroundColor: Colors.white,
-          elevation: 0,
-          titleTextStyle: TextStyle(
-            color: Colors.black,
-            fontSize: Sizes.size18,
-            fontWeight: FontWeight.w600,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          primaryColor: const Color(0xFFE9435A),
+          textSelectionTheme: const TextSelectionThemeData(
+            cursorColor: Color(0xFFE9435A),
+          ),
+          // NOTE: 모든 Scaffold의 앱바에 적용되는 테마
+          appBarTheme: const AppBarTheme(
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.white,
+            elevation: 0,
+            titleTextStyle: TextStyle(
+              color: Colors.black,
+              fontSize: Sizes.size18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          bottomAppBarTheme: const BottomAppBarTheme(
+            color: Colors.white,
+            elevation: 2,
           ),
         ),
-        bottomAppBarTheme: const BottomAppBarTheme(
-          color: Colors.white,
-          elevation: 2,
-        ),
-      ),
-      // NOTE: screen-based 보다는 feature-based 개발이 좋음 (feature can have multiple screens)
-      // home: const SignupScreen(),
-      home: const MainNavigationScreen(),
-    );
+        // NOTE: screen-based 보다는 feature-based 개발이 좋음 (feature can have multiple screens)
+        // home: const SignupScreen(),
+        // home: const MainNavigationScreen(),
+        initialRoute: "/",
+        routes: {
+          "/": (context) => const SignupScreen(),
+          "/discover": (context) => const MainNavigationScreen(),
+        });
   }
 }
